960562fafdf01e91331a0f7e534cca7bdc7a7568
LX-LXF <2397784999@qq.com>
2019-07-08 17:14:03 +0800

    实现HeroList

--- src/main.js
+++ src/main.js
@@ -7,7 +7,11 @@ Vue.config.productionTip = false
 //导入样式文件
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../src/assets/css/index.css'
+
+// 配置路由
 import VueRouter from "vue-router"
+
+import HeroList from './views/heroes/HeroList.vue'
 
 
 Vue.use(VueRouter)
@@ -15,7 +19,8 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes:[
-
+    {name :'home' , path :'/' , redirect: '/heroes'},
+    {name : 'heroes' , path:'/heroes' , component :HeroList }
   ]
 })
 
