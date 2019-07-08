e3367246bc138b1402290d229a6eb8a3e824ec10
LX-LXF <2397784999@qq.com>
2019-07-08 16:54:52 +0800

    配置路由vue-router

--- src/main.js
+++ src/main.js
@@ -7,8 +7,19 @@ Vue.config.productionTip = false
 //导入样式文件
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../src/assets/css/index.css'
+import VueRouter from "vue-router"
 
+
+Vue.use(VueRouter)
+
+
+const router = new VueRouter({
+  routes:[
+
+  ]
+})
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
