794b7e34be34a2837104f29967c341f642e77d16
LX-LXF <2397784999@qq.com>
2019-07-08 20:48:21 +0800

    完成了修改

--- src/views/heroes/HeroEquips.vue
+++ src/views/heroes/HeroEquips.vue
@@ -1,63 +1,78 @@
 <template>
-    <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+  <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
     <h2 class="sub-header">编辑英雄</h2>
     <form>
       <div class="form-group">
         <label for="txtName">姓名</label>
-        <input type="text" class="form-control" v-model="formData.name" id="txtName" placeholder="姓名" />
+        <input
+          type="text"
+          class="form-control"
+          v-model="formData.name"
+          id="txtName"
+          placeholder="姓名"
+        />
       </div>
       <div class="form-group">
         <label for="sex">性别</label>
-        <select class="form-control" v-model="formData.gender"> 
-            <option value="男">男</option>
-            <option value="女">女</option>
+        <select class="form-control" v-model="formData.gender">
+          <option value="男">男</option>
+          <option value="女">女</option>
         </select>
       </div>
       <!-- prevent 阻止默认行为的执行 -->
-      <button class="btn btn-success">提交</button>
+      <button class="btn btn-success" @click.prevent="updata">提交</button>
     </form>
   </div>
 </template>
 
 <script>
-
-
-import axios from 'axios'
-
+import axios from "axios";
 
 // 在组件中获取路由参数 $router.params.id
 export default {
-    props:['id'],
-    data() {
-        return {
-            formData:{
-                name:'',
-                gender:'男'
-            }
+  props: ["id"],
+  data() {
+    return {
+      formData: {
+        name: "",
+        gender: "男"
+      }
+    };
+  },
+  // 组件加载完毕
+  mounted() {
+    this.loadDataByid();
+  },
+  methods: {
+    // 根据id加载英雄数据
+    loadDataByid() {
+      // 发送请求
+      axios.get(`http://localhost:3000/heroes/${this.id}`).then(response => {
+        const { data, status } = response;
+        if (status === 200) {
+          // 存储数据
+          this.formData = data;
         }
+      });
     },
-    // 组件加载完毕
-    mounted(){
-        this.loadDataByid()
+    updata() {
+        // 点击提交按钮
+        axios
+            .put(`http://localhost:3000/heroes/${this.id}` , this.formData)
+            .then((response) =>{
+                const status = response.status
+                if(status === 200) {
+                    this.$router.push({name:'heroes'})
+                }else{
+                    alert('失败')
+                }
+            })
+        // alert(123)
+
     },
-    methods:{
-        // 根据id加载英雄数据
-        loadDataByid(){
-            // 发送请求
-            axios
-                .get(`http://localhost:3000/heroes/${this.id}`)
-                .then((response) =>{
-                    const {data , status} = response
-                    if(status === 200) {
-                        // 存储数据
-                        this.formData = data
-                    }
-                })
-        }
-    }
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
